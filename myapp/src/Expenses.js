5da73fc3ed6c0ca53c94ad758da7e62b11500dcd
[NHAILE] <[natitadd@yahoo.com]>
2019-12-01 19:48:31 -0500

    Tabble is add to Expenses page

--- myapp/src/Expenses.js
+++ myapp/src/Expenses.js
@@ -1,27 +1,62 @@
 import React, { Component } from 'react'
 import Datepicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
-import {Form,Container,FormGroup,Button} from 'reactstrap'
+import {Form,Container,FormGroup,Button,Table} from 'reactstrap'
 import {Link} from 'react-router-dom'
 import Catagory from './Catagory';
 export default class Expenses extends Component {
-    state={
-        date:new Date,
-        isLoading:true,
-        expenses:[],
-        Catagories:[]
+    // {
+    //     "id": 100,
+    //     "expensedate": "2019-06",
+    //     "descript": "NY",
+    //     "location": "New York",
+    //     "catagory": {
+    //         "id": 10,
+    //         "name": "Travel"
+    //     }
+    // }
+
+    emptyItem={
+        id:103,
+        expensedate:new Date(),
+        descript:'Dinner',
+        location:'',
+        catagory:[1,'Travel']
     }
+
+    constructor(props){
+        super(props);
+        this.state={
+            date:new Date,
+        isLoading:true,
+        Expenses:[],
+        Catagories:[],
+        item:this.emptyItem
+        }
+
+    }
+
     async componentDidMount(){
         const response= await fetch('/api/catagories');
         const body=await response.json();
         console.log(body);
-        
-        
         this.setState({
             Catagories:body,
             isLoading:false
-        })
-    }
+        });
+
+        const responseExp= await fetch('/api/expenses');
+        const bodyExp=await responseExp.json();
+        console.log(bodyExp);
+        
+        this.setState({
+            Expenses:bodyExp,
+            isLoading:false
+        });
+        
+   }
+
+    
 
 
     handleChange=()=>{
@@ -29,13 +64,24 @@ export default class Expenses extends Component {
     }
     render() {
         const title=<h3>Add Expense</h3>;
-        const {Catagories, isLoading}=this.state;
+        const {Catagories}=this.state;
+        const {Expenses,isLoading}=this.state;
         if(isLoading){
             return <div>Loading...</div>
         }
         let optionList= Catagories.map(catagory=>
             <option id={catagory.id}>{catagory.name}</option>
         )
+        let rows=Expenses.map(expense=>
+            <tr>
+                <td>{expense.descript}</td>
+                <td>{expense.location}</td>
+                <td>{expense.expensedate}</td>
+                <td>{expense.catagory.name}</td>
+                <td><Button size="sm" color="dangar" onClick={()=>this.remove(expense.id)}/></td>
+            </tr>
+           )
+
         return (
             <div>
                 
@@ -80,6 +126,29 @@ export default class Expenses extends Component {
 
                     </Form>
                 </Container>
+
+                {` `}
+                <Container>
+                    <h3>Exepense List</h3>
+                    <Table className="mt-4">
+                    <thead>
+                        <tr>
+                            <th width="20%">Description</th>
+                            <th width="10%">Location</th>
+                            <th >Catagory</th>
+                            <th width="10%">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+
+                    </tbody>
+
+                    </Table>
+
+
+                </Container>
+
             </div>
         )
     }
